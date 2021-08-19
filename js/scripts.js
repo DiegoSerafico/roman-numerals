a81959be53e5c78ca13ab492af95b21d770e7780
DiegoSerafico <dseraficohernandez@gmail.com>
2021-08-19 11:37:07 -0700

    add completed second test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 function romanize (number) {
-  if (isNaN(number)) {
+  if (isNaN(parseInt(number))) {
     return NaN;
+  }
+  var romanBasics = {M:1000, D:500, C:100, L:50, X:10, V:5, I:1}, roman = "", i;
+  for (i in romanBasics) {
+    if (number >= romanBasics[i]) {
+      roman += i;
+      return roman;
+    }
   }
 }
